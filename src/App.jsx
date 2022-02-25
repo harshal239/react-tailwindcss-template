00703c70cc9588d17afd017db4845bb279372b82
harshal239 <harshalwalunj49@gmail.com>
2022-02-26 00:17:32 +0530

    Initialize Vite React App with TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl underline h-screen flex justify-center items-center">
+      This is a clean React TailwindCSS Template
+    </div>
+  );
+}
+
+export default App;
